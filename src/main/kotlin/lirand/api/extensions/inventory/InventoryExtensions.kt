d57615f6f833ad047872263e8ada1acbc8d4e892
dyam0 <rikkanavo@gmail.com>
2022-04-07 01:40:08 +0400

    Added Inventory clone extension function. Changed the signature of the Inventory factory functions.

--- src/main/kotlin/lirand/api/extensions/inventory/InventoryExtensions.kt
+++ src/main/kotlin/lirand/api/extensions/inventory/InventoryExtensions.kt
@@ -8,26 +8,26 @@ import org.bukkit.inventory.InventoryHolder
 import org.bukkit.inventory.ItemStack
 
 fun Inventory(
-	owner: InventoryHolder? = null,
 	type: InventoryType,
+	owner: InventoryHolder? = null,
 	title: String? = null
-): Inventory = Inventory<Inventory>(owner, type, title)
+): Inventory = Inventory<Inventory>(type, owner, title)
 
 @JvmName("TypedInventory")
 fun <I : Inventory> Inventory(
-	owner: InventoryHolder? = null,
 	type: InventoryType,
+	owner: InventoryHolder? = null,
 	title: String? = null
 ): I {
 	return if (title != null)
-		(server.createInventory(owner, type, title) as I)
+		server.createInventory(owner, type, title) as I
 	else
-		(server.createInventory(owner, type) as I)
+		server.createInventory(owner, type) as I
 }
 
 fun Inventory(
-	owner: InventoryHolder? = null,
 	size: Int,
+	owner: InventoryHolder? = null,
 	title: String? = null
 ): Inventory {
 	return if (title != null)
@@ -36,24 +36,46 @@ fun Inventory(
 		server.createInventory(owner, size)
 }
 
+
 val Inventory.hasSpace: Boolean
 	get() = contents.any { it == null || it.type == Material.AIR }
 
 fun Inventory.hasSpace(
-	item: ItemStack,
-	amount: Int = item.amount
-): Boolean = getSpaceOf(item) >= amount
+	itemStack: ItemStack,
+	amount: Int = itemStack.amount
+): Boolean = getSpaceOf(itemStack) >= amount
 
-fun Inventory.getSpaceOf(item: ItemStack): Int {
+fun Inventory.getSpaceOf(itemStack: ItemStack): Int {
 	return contents.filterNotNull().map {
-		if (it.amount < it.maxStackSize && it.isSimilar(item))
+		if (it.amount < it.maxStackSize && it.isSimilar(itemStack))
 			it.maxStackSize - it.amount
 		else 0
 	}.count()
 }
 
+
 operator fun Inventory.get(slot: Int): ItemStack? = getItem(slot)
 
 operator fun Inventory.set(slot: Int, itemStack: ItemStack?) {
 	setItem(slot, itemStack)
+}
+
+
+fun <I : Inventory> I.clone(
+	cloneItemStacks: Boolean = true,
+	owner: InventoryHolder? = holder,
+	title: String? = null
+): I {
+	val inventory = if (type == InventoryType.CHEST)
+		Inventory(size, owner, title) as I
+	else
+		Inventory<I>(type, owner, title)
+
+	inventory.contents = if (cloneItemStacks)
+		contents.map { it.clone() }.toTypedArray()
+	else
+		contents
+
+
+	return inventory
 }
