1b31b4e546bfeee22e006c90d96cdc530b1d96f3
dyam0 <rikkanavo@gmail.com>
2022-04-12 21:12:12 +0400

    Added Block.state builder extension function. The lambda parameter of ItemStack.meta extension function is no longer crossinline.

--- src/main/kotlin/lirand/api/extensions/world/BlockExtensions.kt
+++ src/main/kotlin/lirand/api/extensions/world/BlockExtensions.kt
@@ -2,6 +2,7 @@ package lirand.api.extensions.world
 
 import org.bukkit.Material
 import org.bukkit.block.Block
+import org.bukkit.block.BlockState
 import org.bukkit.block.data.BlockData
 import org.bukkit.entity.Player
 
@@ -13,6 +14,14 @@ operator fun Block.component3() = z
 fun Block.equalsType(block: Block) = type == block.type
 fun Block.equalsType(data: BlockData) = type == data.material && blockData == data
 fun Block.equalsType(material: Material) = type == material
+
+
+@JvmName("typedState")
+inline fun <reified T : BlockState> Block.state(builder: T.() -> Unit) = apply {
+	(state as? T)?.apply(builder)?.update(true)
+}
+
+inline fun Block.state(builder: BlockState.() -> Unit) = state<BlockState>(builder)
 
 
 fun Block.sendBlockChange(
