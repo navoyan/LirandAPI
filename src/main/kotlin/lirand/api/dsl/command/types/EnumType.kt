3b430b41aa872bf71df30f22191acf3f88aeb8fd
dyamo <rikkanavo@gmail.com>
2022-10-05 23:49:14 +0400

    Added EnumType. MaterialType and ParticleType are just special cases of EnumType now.

---
+++ src/main/kotlin/lirand/api/dsl/command/types/EnumType.kt
@@ -0,0 +1,83 @@
+package lirand.api.dsl.command.types
+
+import com.mojang.brigadier.Message
+import com.mojang.brigadier.StringReader
+import com.mojang.brigadier.context.CommandContext
+import com.mojang.brigadier.suggestion.Suggestions
+import com.mojang.brigadier.suggestion.SuggestionsBuilder
+import lirand.api.dsl.command.types.exceptions.ChatCommandExceptionType
+import lirand.api.dsl.command.types.exceptions.ChatCommandSyntaxException
+import lirand.api.dsl.command.types.extensions.readUnquoted
+import net.md_5.bungee.api.chat.TranslatableComponent
+import org.bukkit.entity.Player
+import java.util.concurrent.CompletableFuture
+
+/**
+ * A [Enum] type.
+ */
+open class EnumType<T : Enum<T>>(
+	enumClass: Class<T>,
+	open val allowedConstants: (sender: Player?) -> Map<T, Message?> = { enumClass.enumConstants.associateWith { null } },
+	open val notFoundExceptionType: ChatCommandExceptionType = ChatCommandExceptionType {
+		TranslatableComponent("argument.id.unknown", it[0])
+	}
+) : WordType<T> {
+
+	private val enumConstants = enumClass.enumConstants.associateBy { it.name.lowercase() }
+
+
+	/**
+	 * Returns a [T] from the result of the [allowedConstants]
+	 * which key matches the string returned by the given [reader].
+	 *
+	 * @param reader the reader
+	 * @return a [T] with the given key
+	 * @throws ChatCommandSyntaxException if a [T] with the given key does not exist
+	 */
+	override fun parse(reader: StringReader): T {
+		val name = reader.readUnquoted().lowercase()
+
+		return enumConstants[name]?.takeIf { it in allowedConstants(null) }
+			?: throw notFoundExceptionType.createWithContext(reader, name)
+	}
+
+	/**
+	 * Returns the [T] constants from the result of the [allowedConstants]
+	 * that start with the remaining input of the given [builder].
+	 *
+	 * @param S the type of the source
+	 * @param context the context
+	 * @param builder the builder
+	 * @return the [T] constants that start with the remaining input
+	 */
+	override fun <S> listSuggestions(
+		context: CommandContext<S>,
+		builder: SuggestionsBuilder
+	): CompletableFuture<Suggestions> {
+		val sender = context.source as? Player? ?: return builder.buildFuture()
+
+		allowedConstants(sender).mapKeys { (constant, _) -> constant.name.lowercase() }
+			.filterKeys { it.startsWith(builder.remaining, true) }
+			.forEach { (constant, tooltip) ->
+				if (tooltip != null)
+					builder.suggest(constant, tooltip)
+				else
+					builder.suggest(constant)
+			}
+
+		return builder.buildFuture()
+	}
+
+	override fun getExamples(): List<String> = enumConstants.keys.toList()
+
+
+
+	companion object {
+		inline fun <reified T : Enum<T>> build(
+			noinline allowedConstants: (sender: Player?) -> Map<T, Message?> = { enumValues<T>().associateWith { null } },
+			notFoundExceptionType: ChatCommandExceptionType = ChatCommandExceptionType {
+				TranslatableComponent("argument.id.unknown", it[0])
+			}
+		) = EnumType(T::class.java, allowedConstants, notFoundExceptionType)
+	}
+}
